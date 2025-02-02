181fe77fbff8a1f43188d6ae6cb3ba8833655f29
Mobadru <tallbadru@gmail.com>
2025-02-02 04:12:51 -0800

    asgi uvocorn

--- BabuRentalHouse/asgi.py
+++ BabuRentalHouse/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BabuRentalHouse.settings')
+settings_module = "BabuRentalHouse.settings" if "RENDER_EXTERNAL_HOSTNAME" in os.environ else "BabuRentalHouse.settings"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',  settings_module)
+
 
 application = get_asgi_application()
