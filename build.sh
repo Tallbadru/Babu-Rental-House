a161706f49b26ebe7c7674871a0f2142f0b9a53b
Mobadru <tallbadru@gmail.com>
2025-02-02 06:28:18 -0800

    css collect static  update 2

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ set -o errexit
 
 pip install -r requirements.txt 
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
 python manage.py migrate 
 if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --noinput
+    python manage.py createsuperuser --no-input
 fi
