cf87a9999596e1b25788cbe05555ff1ceab594b6
Mobadru <tallbadru@gmail.com>
2025-02-02 06:51:40 -0800

    checking

--- BabuRentalHouse/wsgi.py
+++ BabuRentalHouse/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 settings_module = "BabuRentalHouse.settings" if "RENDER_EXTERNAL_HOSTNAME" in os.environ else "BabuRentalHouse.settings"
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',  settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 from django.core.wsgi import get_wsgi_application
 
