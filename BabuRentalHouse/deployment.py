57320e5a7ccdfdd6b42a0ced82a1a2bbaa4dd7a7
Mobadru <tallbadru@gmail.com>
2025-02-02 09:57:25 -0800

    security

--- BabuRentalHouse/deployment.py
+++ BabuRentalHouse/deployment.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware"
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
